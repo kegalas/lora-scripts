4c75fd7a2f6a0bc80d380f34eeac01813f2edeaf
kegalas <63504837+kegalas@users.noreply.github.com>
2023-02-28 15:25:23 +0800

    Update install.bash

--- install.bash
+++ install.bash
@@ -3,9 +3,9 @@ source venv/bin/activate
 
 echo "Installing deps..."
 cd ./sd-scripts
-pip3 install torch==1.13.1+cu117 torchvision==0.14.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
+pip3 install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
 pip3 install --upgrade -r requirements.txt
-pip3 install git+https://github.com/facebookresearch/xformers.git
+pip3 install -U -I --no-deps https://github.com/C43H66N12O12S2/stable-diffusion-webui/releases/download/linux/xformers-0.0.14.dev0-cp310-cp310-linux_x86_64.whl
 pip3 install triton==2.0.0.dev20221202
 pip3 install --upgrade lion-pytorch
 
